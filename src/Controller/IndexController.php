bcba5446f4ffb3ea0be7411173532c82b428d2eb
Liam cariou <cariou.liam@orange.fr>
2024-04-25 12:03:25 +0200

    page posts

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
